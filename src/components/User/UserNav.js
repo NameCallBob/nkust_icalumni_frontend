c7e5cea7791ef2b7f9adf8ef2250488f9c40e21d
NameCallBob <robin92062574@google.com>
2024-09-24 09:15:45 +0800

    only change some code on Nav and Nav css

--- src/components/User/UserNav.js
+++ src/components/User/UserNav.js
@@ -9,38 +9,35 @@ import "css/Navlogo.css"
  */
 function UserNav() {
     return (
-        <Container fluid>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/" className="d-flex align-items-center mx-3">
-                    <img
-                        src={logo}
-                        className="d-inline-block align-top logo-img"
-                        alt="智商系友會LOGO"
-
-                    />
-
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-                    <Nav className="ml-auto">
-                        <NavDropdown title="系友會介紹" id="basic-nav-dropdown" className='mr-auto'>
-                            <NavDropdown.Item href="/IC/intro">簡介</NavDropdown.Item>
-                            <NavDropdown.Item href="/IC/rule">章程</NavDropdown.Item>
-                            <NavDropdown.Item href="/IC/member">組織</NavDropdown.Item>
-                            <NavDropdown.Item href="/IC/joinUs">入會方式</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="/alumniList" className="mx-3">系友們</Nav.Link>
-                        <Nav.Link href="/search" className="mx-3">公司查詢</Nav.Link>
-                        <Nav.Link href="/Recruitment" className="mx-3">徵才啟示</Nav.Link>
-                        <NavDropdown title="系友專區" id="basic-nav-dropdown" className='mr-auto'>
-                            <NavDropdown.Item href="/login">登入</NavDropdown.Item>
-                            <NavDropdown.Item href="/User">使用說明</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
-    );
+<Container fluid>
+    <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/" className="d-flex align-items-center mx-3">
+            <img
+                src={logo}
+                className="d-inline-block align-top logo-img"
+                alt="智商系友會LOGO"
+            />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="ml-auto align-items-center">
+                <NavDropdown title="系友會介紹" id="basic-nav-dropdown" className="mx-3 border-item">
+                    <NavDropdown.Item href="/IC/intro">簡介</NavDropdown.Item>
+                    <NavDropdown.Item href="/IC/rule">章程</NavDropdown.Item>
+                    <NavDropdown.Item href="/IC/member">組織</NavDropdown.Item>
+                    <NavDropdown.Item href="/IC/joinUs">入會方式</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/alumniList" className="mx-3 border-item">系友們</Nav.Link>
+                <Nav.Link href="/search" className="mx-3 border-item">公司查詢　</Nav.Link>
+                <Nav.Link href="/Recruitment" className="mx-3 border-item">徵才啟示　</Nav.Link>
+                <NavDropdown title="系友專區" id="basic-nav-dropdown" className="mx-3 border-item">
+                    <NavDropdown.Item href="/login">登入</NavDropdown.Item>
+                    <NavDropdown.Item href="/User">使用說明</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+</Container>    );
 }
 
 export default UserNav;
